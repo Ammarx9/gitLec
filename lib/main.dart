2635d2c71251f0f7923740a57ab140a94370987c
muhannad <mohndalzaher@gmail.com>
2024-09-08 11:32:37 +0300

    bye

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,13 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:git_lec/screan_one.dart';
 
-void main(){
-
+void main() {
   runApp(HomeApp());
 }
 
+class HomeApp extends StatelessWidget {
+  const HomeApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
